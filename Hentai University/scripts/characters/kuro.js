a8f6efe00e2422eafdccd7c63ce9e8cd8ff438ec
Brandon <corenkeitaro@gmail.com>
2019-10-30 15:37:14 -0700

    Update kuro.js

--- Hentai University/scripts/characters/kuro.js
+++ Hentai University/scripts/characters/kuro.js
@@ -587,7 +587,7 @@ function writeEvent(name) { //Plays the actual event.
 			}
 			writeText("She double-checks her clothes for any leftover jizz on her before she gets up and leaves, winking to you just before slipping through the door.");
 			if (data.player.location != 'gallery') {
-				if(checkTrust('kuro') < 22){
+				if(checkTrust('kuro') < 24){
 					raiseTrust('kuro', 1);
 				}
 				data.player.location = "playerOffice";
